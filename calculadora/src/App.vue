23ecbeda76e42b5973fcb7bb6172412684d9a757
Elihú <a23elivalyen@inspedralbes.cat>
2025-05-22 13:56:19 +0200

    dada

---
+++ calculadora/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="calculator">
+    <button
+      v-for="number in numbers"
+      :key="number"
+      @click="showNumber(number)"
+    >
+      {{ number }}
+    </button>
+    <p v-if="selected">Has clicado el número: {{ selected }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      selected: null,
+    };
+  },
+  methods: {
+    showNumber(n) {
+      this.selected = n;
+    },
+  },
+};
+</script>
+
+<style>
+.calculator {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+  width: 200px;
+  margin: 20px auto;
+}
+button {
+  padding: 20px;
+  font-size: 1.5rem;
+}
+</style>
